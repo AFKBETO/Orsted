d5040531398710aab52265acc58d1c6bed5f3fbe
QV NGUYEN <qv.nguyen00@gmail.com>
2022-07-22 11:37:05 +0200

    set up commands

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,7 +1,13 @@
 import { Interaction, InteractionType } from 'discord.js'
+import { CommandList } from '../commands/_CommandList'
 
 export const onInteraction = async (interaction: Interaction) => {
   if (interaction.type === InteractionType.ApplicationCommand) {
-    
+    for (const Command of CommandList) {
+      if (interaction.commandName === Command.data.name) {
+        await Command.run(interaction)
+        break
+      }
+    }
   }
 }
---
+++ src/interfaces/CommandInt.ts
@@ -0,0 +1,6 @@
+import { CommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js'
+
+export interface CommandInt {
+  data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder
+  run: (interaction: CommandInteraction) => Promise<void>
+}
