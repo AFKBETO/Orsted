4ac6940cbfb19d8237a01c3f653c96bc62b88f9d
QV NGUYEN <qv.nguyen00@gmail.com>
2022-07-22 11:25:49 +0200

    initial setups

---
+++ src/events/onInteraction.ts
@@ -0,0 +1,7 @@
+import { Interaction, InteractionType } from 'discord.js'
+
+export const onInteraction = async (interaction: Interaction) => {
+  if (interaction.type === InteractionType.ApplicationCommand) {
+    
+  }
+}
