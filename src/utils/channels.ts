d91fbb4caf63bafe6ed14e7261ffb6c6ee362aa2
QV NGUYEN <qv.nguyen00@gmail.com>
2022-07-27 21:35:59 +0200

    fix shame Id

--- src/utils/channels.ts
+++ src/utils/channels.ts
@@ -1,4 +1,4 @@
 export const eventLogId = '824175906120663060'
 export const hotSauceId = '814172539290648636'
 export const looperId = '908073828557672588'
-export const shameId = '926610864612724757s'
+export const shameId = '926610864612724757'
