ec5c73a7435c2c590ab843513b07652485b61c27
QV NGUYEN <qv.nguyen00@gmail.com>
2022-08-02 11:36:13 +0200

    add orsted-certified in magic number

--- src/commands/magicNumber.ts
+++ src/commands/magicNumber.ts
@@ -1,6 +1,6 @@
-import { EmbedBuilder, SlashCommandBuilder, TextChannel } from 'discord.js'
+import { CommandInteractionOptionResolver, EmbedBuilder, SlashCommandBuilder, TextChannel } from 'discord.js'
 import { CommandInt } from '../interfaces/CommandInt'
-import { eventLogId, hotSauceId } from '../utils/channels'
+import { eventLogId, hotSauceId, orstedHotId } from '../utils/channels'
 
 export const magicNumber: CommandInt = {
   data: new SlashCommandBuilder()
@@ -11,13 +11,25 @@ export const magicNumber: CommandInt = {
         .setName('number')
         .setDescription('Magic number to post')
         .setRequired(true)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName('orstedcertified')
+        .setDescription('Is this sauce Orsted-certified?')
     ) as SlashCommandBuilder,
   run: async (interaction) => {
     try {
       await interaction.deferReply({ ephemeral: true })
       const eventLog = interaction.guild?.channels.cache.get(eventLogId) as TextChannel
       const { user } = interaction
-      const magicnumber = interaction.options.get('number', true).value
+      const magicnumber = (interaction.options as CommandInteractionOptionResolver).getNumber('magicnumber', true)
+      const orstedCertified = (interaction.options as CommandInteractionOptionResolver).getBoolean('orstedcertified')
+      let hotSauce: TextChannel
+      if (orstedCertified) {
+        hotSauce = interaction.guild?.channels.cache.get(orstedHotId) as TextChannel
+      } else {
+        hotSauce = interaction.guild?.channels.cache.get(hotSauceId) as TextChannel
+      }
       const url = `https://nhentai.net/g/${magicnumber}`
       const log = await eventLog.send({ content: url })
       setTimeout(async () => {
